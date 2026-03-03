7aac2ac819918fe14e498261073e4de8baba226b
Tushar Sahu <tsahu18jsr@gmail.com>
2026-03-03 12:36:12 +0530

    Landing Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { UserButton} from "@clerk/nextjs";
+import Header from  "./_components/Header";
+import Hero from "./_components/Hero";
+import CourseList from "./_components/CourseList";
 
 export default function Home() {
   return (
-    <div className="p-8">
-      <div className="flex justify-between items-center mb-8">
-        <h2 className="text-2xl font-bold">Subscribe to my channel</h2>
-        <UserButton/>
-      </div>
-      <Button>Subscribe</Button>
+    <div>
+      <Header />
+      <Hero />
+      <CourseList />
+    
+      <div
+        className="absolute -bottom-40 -left-40
+ h-[500px] w-[500px] bg-purple-400/20 blur-[120px] rounded-full"
+      />
+      <div
+        className="absolute top-20 left-1/3 bottom-[-200px]
+ h-[500px] w-[500px] bg-pink-400/20 blur-[120px] rounded-full"
+      />
+      <div
+        className="absolute bottom-[-200px] left-1/3
+ h-[500px] w-[500px] bg-blue-400/20 blur-[120px] rounded-full"
+      />
+      <div
+        className="absolute top-[200px] left-1/2
+ h-[500px] w-[500px] bg-sky-400/20 blur-[120px] rounded-full"
+      />
+    
     </div>
   );
 }
