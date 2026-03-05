bc8c98faa5e2204e6eb785c4b10b6a68dd3e17e1
Tushar Sahu <tsahu18jsr@gmail.com>
2026-03-05 23:15:34 +0530

    Add production config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coursevideogenerator.blob.core.windows.net',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
